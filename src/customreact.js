207d8464fd513c712a7d4f3729649f026638308f
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-10-20 00:01:14 +0530

    how to work react element work behind the seen

--- src/customreact.js
+++ src/customreact.js
@@ -1,14 +1,20 @@
 const mainContainer = document.getElementById("root");
-
 function customRender(reactElement, container) {
+  /*
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute("href", reactElement.props.href);
   domElement.setAttribute("target", reactElement.props.target);
   container.appendChild(domElement);
+  */
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === 'children') continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
 }
-
-
 const reactElement = {
   type: "a",
   props: {
@@ -17,5 +23,4 @@ const reactElement = {
   },
   children: "Click me to visit google",
 };
-
 customRender(reactElement, mainContainer);
