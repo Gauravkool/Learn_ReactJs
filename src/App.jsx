3858cc9139f948013ba0efe38d7dcfd4777d6b37
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-10-19 09:18:10 +0530

    Initial project and Creat cutome react setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-red-700">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/customreact.js
@@ -0,0 +1,21 @@
+const mainContainer = document.getElementById("root");
+
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+}
+
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+customRender(reactElement, mainContainer);
