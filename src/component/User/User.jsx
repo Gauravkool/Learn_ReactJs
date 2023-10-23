ea30dd65734a350cec0f3e1a2dc380eb45aa0d51
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-10-23 12:30:26 +0530

    fix params id issue

--- src/component/User/User.jsx
+++ src/component/User/User.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 function User() {
-  const userId = useParams();
+  const {userId} = useParams();
   console.log("userId", userId);
-  return <>User: {userId}</>;
+  return <div className="text-center bg-gray-500 text-white text-2xl py-4">
+    User : {userId}
+  </div>
 }
 export default User;
